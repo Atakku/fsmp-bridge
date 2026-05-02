a8e7fc72a00dde9d7d1976d13cf0bb97c6a80cfe
Atakku <atakkudev@gmail.com>
2026-05-02 18:14:34 +0200

    mrowww

--- java/server/zone/hrt/bridge/mixin/MixinServerPlayer.java
+++ java/server/zone/hrt/bridge/mixin/MixinServerPlayer.java
@@ -4,12 +4,14 @@
 
 package zone.hrt.bridge.mixin;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import zone.hrt.bridge.Bridge;
 
@@ -19,5 +21,10 @@ abstract class MixinServerPlayer {
   private void die(DamageSource source, CallbackInfo ci) {
     ServerPlayer spe = (ServerPlayer) (Object) this;
     Bridge.onPlayerDeath(spe, source);
+  }
+
+  @ModifyVariable(at = @At("HEAD"), method = "Lnet/minecraft/server/level/ServerPlayer;adjustSpawnLocation(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/core/BlockPos;")
+  private BlockPos getSharedSpawnPos(BlockPos oldPos) {
+    return Bridge.LOC_CACHE.getOrDefault(((ServerPlayer) (Object) this).getUUID(), oldPos);
   }
 }
